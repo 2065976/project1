c24d9231dcd42a1379bb7a54a6cb56ab672e1b3b
2065976 <2065976@talnet.nl>
2020-10-02 21:36:40 +0200

    New queries with statements added

--- sql_statements.sql
+++ sql_statements.sql
@@ -15,7 +15,7 @@ CREATE TABLE account(
 -- Create table 'person' with ID as foreign key from table 'account'
 CREATE TABLE person(
   id INT NOT NULL AUTO_INCREMENT,
-  username VARCHAR(255) NOT NULL,
+  username VARCHAR(255) UNIQUE NOT NULL,
   firstname VARCHAR(255) NOT NULL,
   middlename VARCHAR(255),
   lastname VARCHAR(255) NOT NULL,
@@ -24,10 +24,43 @@ CREATE TABLE person(
   FOREIGN KEY(account_id) REFERENCES account(id)
 );
 
+-- Create table 'usertype' with ID as primary key
+CREATE TABLE usertype (
+  id INT NOT NULL AUTO_INCREMENT,
+  type VARCHAR(255),
+  created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY(id)
+);
+
+-- Add  column 'created' and 'last_updated' to table 'person'
+ALTER TABLE person
+  ADD created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  ADD updated DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+
+-- Add  column 'created' and 'last_updated' to table 'account'
+ALTER TABLE account
+  ADD created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  ADD updated DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+
+-- Add column 'username' and foreign key for 'usertype_id' to table 'account'
+ALTER TABLE account
+  ADD username VARCHAR(255) UNIQUE NOT NULL AFTER id,
+  ADD usertype_id INT NOT NULL,
+  ADD FOREIGN KEY (usertype_id) REFERENCES usertype(id);
+
+-- Drops column 'username' from table 'person'
+ALTER TABLE person
+  DROP column username;
+
+-- Create usertype 'user' with id '1' in table 'usertype'
+INSERT INTO usertype (id, type, created, updated)
+  VALUES ('1', 'user', NULL, NULL);
+
 -- Create user 'admin' in table 'account'
 INSERT INTO account (id, email, password) 
-VALUES ('1', 'admin@gmail.com', 'Welkom01');
+  VALUES ('1', 'admin@gmail.com', 'Welkom01');
 
 -- Create user 'admin' in table 'person'
 INSERT INTO person (id, username, firstname, lastname, account_id) 
-VALUES ('1', 'admin', 'Mike', 'Wazowski', '1');
+  VALUES ('1', 'admin', 'Mike', 'Wazowski', '1');
