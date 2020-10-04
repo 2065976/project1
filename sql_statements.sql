0f537a61739305634fb7876cb663963a7c3127d5
2065976 <2065976@talnet.nl>
2020-10-04 15:14:08 +0200

    Changed tab spaces (4 -> 2)

--- sql_statements.sql
+++ sql_statements.sql
@@ -6,10 +6,10 @@ USE project1;
 
 -- Create table 'account' with ID as primary key
 CREATE TABLE account(
-	id INT NOT NULL AUTO_INCREMENT,
-	email VARCHAR(255) UNIQUE NOT NULL,
-	password VARCHAR(255) NOT NULL,
-	PRIMARY KEY(id)
+  id INT NOT NULL AUTO_INCREMENT,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  PRIMARY KEY(id)
 );
 
 -- Create table 'person' with ID as foreign key from table 'account'
