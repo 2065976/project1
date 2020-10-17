9f778d7b90dcd5d2120b8b7cdd5d0d40999aa45d
2065976 <2065976@talnet.nl>
2020-10-17 22:53:28 +0200

    Re-designed style & Login function added

--- index.php
+++ index.php
@@ -1,39 +1,56 @@
 <?php
  
-include_once 'database.php';
+ session_start();
 
-$db = new database('localhost', 'root', '', 'project1', 'utf8');
+ include_once 'database.php';
 
+ $alert = "";
+
+   if (isset($_POST['login'])) {
+
+      $alert = $_SESSION['alert'];
+
+      $username = $_POST['username'];
+      $password = $_POST['password'];
+
+      $db = new database("localhost", "root", "", "project1", "utf8");
+
+      $db->login($username, $password); 
+	}
+	
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <link rel="stylesheet" href="css/style.css">
+	<meta charset="UTF-8">
+	<title>Login</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class="wrapper">
-        <h2>Login</h2>
-        <p>Please fill in your credentials to login.</p>
-        <form action="" method="post">
-            <div class="form-group">
-                <label for="username">Username</label>
-                <input type="text" name="username" class="form-control" value="">
-                <span class="help-block"></span>
-            </div>    
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" name="password" class="form-control">
-                <span class="help-block"></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" name="login" value="Login">
-            </div>
-            <p>Don't have an account? <a href="signup.php">Sign up now</a>.</p>
-        </form>
-    </div>
+	<div class="container">
+	<h2>Login</h2><br>
+		<form action="" method="post">
+			<div class="form-group">
+				<label for="username">Username</label>
+				<input type="text" name="username" class="form-control" required>
+				<span class="help-block"></span>
+			</div>    
+			<div class="form-group">
+				<label for="password">Password</label>
+				<input type="password" name="password" class="form-control" required>
+				<span class="help-block"></span>
+			</div>
+			<div class="form-group">
+				<a href="signup.php"><input type="button" class="btn btn-default btn-sm" value="Sign Up"></a>
+				<a href="lostpsw.php"><input type="button" class="btn btn-default btn-sm" value="Recover Password"></a><br><BR>
+				<input type="submit" class="btn btn-light btn-block btn-lg" name="login" value="Login">
+			</div>
+		</form>
+		<?php echo $alert; ?>
+	</div>
 </body>
 </html>
