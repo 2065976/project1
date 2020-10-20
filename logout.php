e420c81b2bffc72e707328c9ee0f2dfcd9db090d
2065976 <2065976@talnet.nl>
2020-10-20 16:40:25 +0200

    Session Destroy added

--- logout.php
+++ logout.php
@@ -5,9 +5,12 @@ session_start();
 unset($_SESSION["username"]);
 unset($_SESSION["password"]);
 
+// Destroy user session
+session_destroy();
+
 // Redirect user to index page
 header("Location: index.php");
 
-exit;
+exit();
 
 ?>
